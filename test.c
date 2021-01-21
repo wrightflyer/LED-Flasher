e12dfe5419ff3ebf23902ae88432a2d4850ca39b
Cliff Lawson <cliff.lawson@continental-corporation.com>
2021-01-21 10:34:51 +0000

    Fixed some problems with delay

--- test.c
+++ test.c
@@ -1,3 +1,4 @@
+#define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
@@ -5,6 +6,6 @@ int main(void) {
     DDRB = 0xFF;
     while(1) {
         PORTB ^= 0xFF;
-        _delay_ms(1);
+        _delay_ms(100);
     }
 }
